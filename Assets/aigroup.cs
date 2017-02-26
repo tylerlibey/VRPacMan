9dd29a22b2ce2be8a86af3253b6d108c7113a047
tylerlibey <tylerlibey@gmail.com>
2017-02-26 13:22:55 -0800

    Created Repo

---
+++ Assets/aigroup.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aigroup : MonoBehaviour {
+
+    public GameObject[] ghosts;
+    private Vector3[] starting;
+
+
+    private float delayTime = 5;
+    private float last = 0;
+    private int active = 0;
+
+	// Use this for initialization
+	void Start () {
+        starting = new Vector3[ghosts.Length];
+        for(int ii = 0; ii < ghosts.Length; ii++)
+        {
+            starting[ii] = ghosts[ii].transform.position;
+            ghosts[ii].GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 0;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > last + delayTime)
+        {
+            activate();
+            last = Time.time;
+        }
+	}
+    private void activate()
+    {
+        if (active < ghosts.Length) {
+            ghosts[active].GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 20;
+            active++;
+        }
+    }
+
+    public void resetGhosts()
+    {
+        print("resetting");
+        for (int ii = 0; ii < ghosts.Length; ii++)
+        {
+            
+            ghosts[ii].GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 0;
+            ghosts[ii].GetComponent<UnityEngine.AI.NavMeshAgent>().Warp(starting[ii]);
+
+        }
+        active = 0;
+        last = Time.time;
+    }
+}
---
+++ Assets/dotPicker.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class dotPicker : MonoBehaviour {
+
+    int score = 0;
+    int lives = 3;
+    public Text scoreText;
+    public aigroup aicontroller;
+    public GameObject player;
+    private Vector3 starting;
+    bool restarting = false;
+    float restartTime = 1;
+    bool gameover = false;
+
+	// Use this for initialization
+	void Start () {
+        starting = player.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (restarting || gameover)
+        {
+            player.transform.position = starting;
+            restartTime -= Time.deltaTime;
+            if (restartTime <= 0)
+            {
+                restartTime = 0;
+                restarting = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "prize")
+        {
+            score++;
+            scoreText.text = "Score " + score + " Lives: " + lives;
+            Destroy(other.gameObject);
+            GetComponent<AudioSource>().Play();
+        }
+        if(other.tag == "ai")
+        {
+            aicontroller.resetGhosts();
+            if (!restarting)
+            {
+                lives--;
+                if (lives == 0)
+                {
+                    scoreText.text = "Game Over!  Score " + score;
+                    gameover = true;
+                }
+                else if (lives > 0)
+                {
+                    scoreText.text = "Score " + score + " Lives: " + lives;
+                    player.transform.position = starting;
+                }
+                restarting = true;
+            }
+        }
+    }
+}
